c6c48cd9f25b8a69e4277a57bb3b11d2ca407cc0
BeNikk <bhattnik442@gmail.com>
2025-10-16 23:36:01 +0530

    init:trpc setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,8 @@
+import { useTRPC } from "@/trpc/client";
 
 export default function Home() {
+  const trpc = useTRPC();
+  trpc.bye.queryOptions({text:"Hello"});
   return (
     <div>
       hello world
