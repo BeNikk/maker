d90bf415e0007f55ee8d12652431d2cdd2652363
BeNikk <bhattnik442@gmail.com>
2025-10-17 13:31:29 +0530

    inngest:bg jobs

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,21 @@
-import Client from "@/components/client";
-import { getQueryClient, trpc } from "@/trpc/server";
-import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
-import { Suspense } from "react";
+"use client";
 
-export default async function Home() {
-  const queryClient = getQueryClient();
-  void queryClient.prefetchQuery(trpc.bye.queryOptions({ text: "nikhil" }));
+import { Button } from "@/components/ui/button";
+import { useTRPC } from "@/trpc/client";
+import { useMutation } from "@tanstack/react-query";
+
+export default  function Home() {
+  const trpc = useTRPC();
+  const invoke = useMutation(trpc.callInngest.mutationOptions({}));
   return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      <Suspense>
-        <Client />
-      </Suspense>
-    </HydrationBoundary>
+    <div>
+      test
+      <Button onClick={()=>{
+        invoke.mutate({email:"nikhil"});
+      }}>
+        Click
+      </Button>
+    </div>
+
   );
 }
--- trpc/routers/_app.ts
+++ trpc/routers/_app.ts
@@ -1,16 +1,31 @@
-import { z } from 'zod';
-import { baseProcedure, createTRPCRouter } from '../init';
+import { z } from "zod";
+import { baseProcedure, createTRPCRouter } from "../init";
+import { inngest } from "@/src/inngest/client";
 export const appRouter = createTRPCRouter({
   bye: baseProcedure
     .input(
       z.object({
         text: z.string(),
-      }),
+      })
     )
     .query((opts) => {
       return {
         greeting: `hello ${opts.input.text}`,
       };
+    }),
+  callInngest: baseProcedure
+    .input(
+      z.object({
+        email: z.string(),
+      })
+    )
+    .mutation(async ({ input }) => {
+      await inngest.send({
+        name: "test/hello.world",
+        data: {
+          email: input.email,
+        },
+      });
     }),
 });
 // export type definition of API
