8ea502b06ac3057378c7dea5b835b99de65ef1bf
BeNikk <bhattnik442@gmail.com>
2025-10-17 23:03:22 +0530

    feat:add tools, workers to create sites in sandbox

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,7 +1,9 @@
 import { inngest } from "./client";
-import { createAgent, gemini } from "@inngest/agent-kit";
+import { createAgent, createNetwork, createTool, gemini } from "@inngest/agent-kit";
 import { Sandbox } from "@e2b/code-interpreter";
-import getSandbox from "./utils";
+import getSandbox, { lastMessageAssistant } from "./utils";
+import { createOrUpdateFiles, readFilesTool, terminalTool } from "./tools";
+import { PROMPT } from "./prompt";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
@@ -13,16 +15,41 @@ export const helloWorld = inngest.createFunction(
       return sbx.sandboxId;
     });
     const coder = createAgent({
-      name: "summariser",
-      system:
-        "You are an expert next js developer. you write readable beautiful maintainable code,you write great code",
-      model: gemini({ model: "gemini-2.5-pro" }),
+      name: "code-agent",
+      description:"Expert, senior coding agent",
+      system:PROMPT,
+      model: gemini({ model: "gemini-2.5-pro"}),
+      tools:[terminalTool(sandboxId),
+        createOrUpdateFiles(sandboxId),
+        readFilesTool(sandboxId),
+      ],
+      lifecycle:{
+        onResponse:async ({result,network})=>{
+            const lastMessage = lastMessageAssistant(result);
+            if(lastMessage && network){
+                if(lastMessage.includes("<task_summary>")){
+                    network.state.data.summary = lastMessage;
+
+                }
+            }
+            return result;
+        }
+      }
     });
 
-    const { output } = await coder.run(
-      `Write the code for the following prompt- ${event.data.value}`
-    );
-    console.log(output);
+    const network = createNetwork({
+        name:"Coding agent network",
+        agents:[coder],
+        maxIter:15,
+        router:async ({network})=>{
+            const summary = network.state.data.summary;
+            if(summary){
+             return;   
+            }
+            return coder;
+        }
+    })
+    const result = await network.run(event.data.value);
     // await step.sleep("wait-a-moment", "10s");
     const sandBoxUrl = await step.run("get-sandbox-url",async()=>{
         const sandbox = await getSandbox(sandboxId);
@@ -31,6 +58,10 @@ export const helloWorld = inngest.createFunction(
     })
     console.log(sandBoxUrl);
 
-    return { output, sandBoxUrl };
+    return { url:sandBoxUrl,
+        title:"Fragment",
+        files:result.state.data.files,
+        summary:result.state.data.summary
+     };
   }
 );
