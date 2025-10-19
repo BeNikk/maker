b97df3baea5359cfb94226571cc25b476f0e6e5b
BeNikk <bhattnik442@gmail.com>
2025-10-19 11:52:23 +0530

    add:message ui

---
+++ components/message-card.tsx
@@ -0,0 +1,94 @@
+import { Fragment, MessageRole, Type } from "@/lib/generated/prisma";
+import { Card } from "./ui/card";
+import { cn } from "@/lib/utils";
+import { format } from "date-fns";
+import Image from "next/image";
+export const MessageCard = ({
+  key,
+  content,
+  role,
+  fragment,
+  createdAt,
+  isActiveFragment,
+  onFragmentClick,
+  type,
+}: {
+  key: string;
+  content: string;
+  role: MessageRole;
+  fragment: Fragment | null;
+  createdAt: Date;
+  isActiveFragment: boolean;
+  onFragmentClick: (fragment: Fragment) => void;
+  type: Type;
+}) => {
+  if (role == "ASSISTANT") {
+    return (
+      <AssistantMessage
+        content={content}
+        fragment={fragment}
+        createdAt={createdAt}
+        isActiveFragment={isActiveFragment}
+        onFragmentClick={onFragmentClick}
+        type={type}
+      />
+    );
+  }
+  return (
+    <div>
+      <UserMessage content={content} />
+    </div>
+  );
+};
+
+const UserMessage = ({ content }: { content: string }) => {
+  return (
+    <div className="flex justify-end pb-4 pr-2 pl-10">
+      <Card className="rounded-lg bg-muted p-3 shadow-none border-none max-w-[80%] break-words">
+        {content}
+      </Card>
+    </div>
+  );
+};
+
+const AssistantMessage = ({
+  content,
+  fragment,
+  createdAt,
+  isActiveFragment,
+  onFragmentClick,
+  type,
+}: {
+  content: string;
+  fragment: Fragment | null;
+  createdAt: Date;
+  isActiveFragment: boolean;
+  onFragmentClick: (fragment: Fragment) => void;
+  type: Type;
+}) => {
+  return (
+    <div
+      className={cn(
+        "flex flex-col group px-2 pb-4 ",
+        type == "ERROR" && "text-red-700 dark:text-red-500"
+      )}
+    >
+      <div className="flex flex-row items-center gap-2 pl-2 mb-2">
+        <Image
+        src={'/logo.svg'}
+        alt ="logo"
+        height={18}
+        width={18}
+        className="shrink-0"
+        />
+        <span className="text-sm font-medium">Maker</span>
+        <span className="text-xs text-muted-foreground opacity-0 transition-opacity group-hover:opacity-100">
+          {format(createdAt, "HH:mm 'on' MMM dd, yyyy ")}
+        </span>
+      </div>
+      <div className="pl-8 flex flex-col gap-y-4">
+        <span>{content}</span>
+      </div>
+    </div>
+  );
+};
--- components/project-view.tsx
+++ components/project-view.tsx
@@ -2,6 +2,9 @@
 
 import { useTRPC } from "@/trpc/client";
 import { useSuspenseQuery } from "@tanstack/react-query";
+import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from "./ui/resizable";
+import { MessageContainer } from "./message-container";
+import { Suspense } from "react";
 
 interface Props {
   projectId: string;
@@ -14,15 +17,28 @@ export const ProjectView = ({ projectId }: Props) => {
       id: projectId,
     })
   );
-  const { data: messages } = useSuspenseQuery(
-    trpc.messages.getMany.queryOptions({
-      projectId: projectId,
-    })
-);
-return (
-    <div>
-        {JSON.stringify(project)}
-        {JSON.stringify(messages)}
+
+  return (
+    <div className="h-screen">
+      <ResizablePanelGroup direction="horizontal">
+        <ResizablePanel
+        defaultSize={35}
+        minSize={20}
+        className="flex flex-col min-h-0"
+        >
+            <Suspense fallback= {<p>Loading...</p>}>
+            <MessageContainer projectId={projectId}/>
+
+            </Suspense>
+        </ResizablePanel>
+        <ResizableHandle withHandle/>
+        <ResizablePanel
+        defaultSize={65}
+        minSize={50}
+        >
+            project preview
+            </ResizablePanel>
+      </ResizablePanelGroup>
     </div>
-)
+  );
 };
