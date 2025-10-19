055c47ac75d56414519b2abbc0dd47e3af2e52b7
BeNikk <bhattnik442@gmail.com>
2025-10-19 09:18:51 +0530

    add:prefetching and displaying content

--- app/project/[projectId]/page.tsx
+++ app/project/[projectId]/page.tsx
@@ -1,4 +1,7 @@
+import { ProjectView } from "@/components/project-view";
 import { getQueryClient, trpc } from "@/trpc/server";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { Suspense } from "react";
 
 const Page = async({params}: {params:Promise<{projectId:string}>})=>{
     const { projectId }= await params;
@@ -6,11 +9,18 @@ const Page = async({params}: {params:Promise<{projectId:string}>})=>{
     void queryClient.prefetchQuery(trpc.messages.getMany.queryOptions({
         projectId:projectId
     }));
-    
+    void queryClient.prefetchQuery(trpc.projects.getOne.queryOptions({
+        id:projectId
+    }));
+
+
     return(
-        <>
-        projectId: {projectId}
-        </>
+        <HydrationBoundary state={dehydrate(queryClient)}>
+            <Suspense fallback={<>Loading</>}> 
+            <ProjectView projectId={projectId} />
+            </Suspense>
+            projectId: {projectId}
+        </HydrationBoundary>
     )
 
 }
